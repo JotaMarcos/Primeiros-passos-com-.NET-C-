154f88abac1c77d880693a6a94bf5b970dfca36a
JotaMarcos <jmarcos_duarte@hotmail.com>
2020-06-25 19:48:02 -0400

    Primeiro contato com Linguagem C# da Plataforma .NET

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DigitalInnovationOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           /* Console.WriteLine("Olá! Mundo do Desenvolvimento .NET!!!");
+            Console.WriteLine("Vamos Aprender e Dominar o .NET!!!");*/
+            int numeroDeVezes = 10;
+
+            for (int i = 0; i < numeroDeVezes; i++)
+            {
+                Console.WriteLine($"Olá, Mundo do Desenvolvimeno .NET!!! {i}");
+            }
+        }
+    }
+}
